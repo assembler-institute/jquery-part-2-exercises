a22f6926180f72c2ba1e02c718774c39a9ddcd18
Dani Lucaci <danilucaci@gmail.com>
2020-04-29 11:49:26 +0200

    fix 06-exercise.test.js

--- src/__tests__/06-exercise.test.js
+++ src/__tests__/06-exercise.test.js
@@ -4,13 +4,14 @@ import exercise06 from "../06-exercise/06-exercise";
 
 describe("06-exercise", () => {
   beforeAll(() => {
-    document.head.innerHTML = ` <style>
+    document.head.innerHTML = `<style>
       .box {
         font-size: 16px;
         border-radius: 4px;
         border: 2px solid lightgray;
         max-width: 8em;
         padding: 24px;
+        display: none;
         background-color: hsl(0, 0%, 98%);
       }
     </style>`;
